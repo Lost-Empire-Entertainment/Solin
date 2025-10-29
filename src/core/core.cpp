1a0b7ef119547c1c54a97e3a3798c8fbfcd34844
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-10-30 00:03:04 +0200

    testing kfont importing

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -3,16 +3,34 @@
 //This is free software, and you are welcome to redistribute it under certain conditions.
 //Read LICENSE.md for more information.
 
+#include <filesystem>
+#include <vector>
+#include <string>
+
+#include "KalaHeaders/log_utils.hpp"
+
 #include "KalaWindow/include/core/core.hpp"
 #include "KalaWindow/include/core/crash.hpp"
 
 #include "core/core_program.hpp"
+#include "core/import_kfont.hpp"
 #include "graphics/render.hpp"
+
+using KalaHeaders::Log;
+using KalaHeaders::LogType;
 
 using KalaWindow::Core::KalaWindowCore;
 using KalaWindow::Core::CrashHandler;
 
+using KalaFont::ImportKalaFont;
+using KalaFont::GlyphResult;
+
 using Solin::Graphics::Render;
+
+using std::filesystem::path;
+using std::filesystem::current_path;
+using std::vector;
+using std::to_string;
 
 namespace Solin::Core
 {
@@ -21,6 +39,21 @@ namespace Solin::Core
 		CrashHandler::Initialize(
 			"Solin IDE",
 			Shutdown);
+			
+		path fontPath = current_path() / "files" / "fonts" / "bw.kfont";
+		vector<GlyphResult> result{};
+		
+		if (!ImportKalaFont(fontPath, result))
+		{
+			KalaWindowCore::ForceClose(
+				"Font error",
+				"Failed to load font from path '" + fontPath.string() + "'!");
+		}
+		
+		Log::Print(
+			"Loaded '" + to_string(result.size()) + "' glyphs from font '" + fontPath.string() + "'!",
+			"LOAD_KFONT",
+			LogType::LOG_SUCCESS);
 		
 		Render::Initialize();
 	}
